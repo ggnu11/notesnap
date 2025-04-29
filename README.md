9c72470006ae69594bf29c1d8030ea72f0749c26
choi0712 <130032473+ggnu11@users.noreply.github.com>
2025-04-29 10:39:28 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://velog.io/@yeon0731/Tailwind-CSS-v4-%EB%8F%84%EC%9E%85%EA%B3%BC-%EC%84%A4%EC%A0%95
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center w-screen h-screen">
+      <h1 className="underline text-banana bg-mint-500">Banana</h1>
+      <p>mint chocolate</p>
+    </main>
+  );
+}
+
+export default App;
